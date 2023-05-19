484c8b62a9049001932c9b8cea6a303954ae9801
AvishkaSooriyapperuma <avishkadilshan400@gmail.com>
2023-05-19 12:08:48 +0530

    terraform install commented

--- Jenkins/jenkinsfile.groovy
+++ Jenkins/jenkinsfile.groovy
@@ -39,15 +39,14 @@ pipeline {
           }     
           }
 
-    stage('install terraform'){
-      steps{
-        script{
-                sh 'sudo apt update;sudo apt install wget'
-                sh 'wget https://releases.hashicorp.com/terraform/0.15.4/terraform_0.15.4_linux_amd64.zip;unzip terraform_0.15.4_linux_amd64.zip -d /usr/local/bin/'
-                sh "terraform version"
-        }
-      }
-    } 
+    // stage('install terraform'){
+    //   steps{
+    //     script{
+    //             sh 'wget https://releases.hashicorp.com/terraform/0.15.4/terraform_0.15.4_linux_amd64.zip;unzip terraform_0.15.4_linux_amd64.zip -d /usr/local/bin/'
+    //             sh "terraform version"
+    //     }
+    //   }
+    // } 
 
     stage('initialize and plan terraform') {
         steps{
